492cb83f6ef9d54473d3d09c14b6110b404fc6b6
Izak Max <j.isaacmaximo@gmail.com>
2023-01-17 18:22:24 -0300

    Inicio do Curso RocketSeat

---
+++ LaunchBase/index.js
@@ -0,0 +1,12 @@
+feather.replace()
+
+const Popup = document.querySelector(".sobreposition")
+
+const Gatilho = document.querySelector("#buytrigger")
+
+
+
+
+Gatilho.addEventListener("click", function(){
+    Popup.classList.add('visible')
+})
